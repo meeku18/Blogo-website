467386c73345eeb7a913c6866ea9de7a00ba3459
meeku18 <97672118+meeku18@users.noreply.github.com>
2024-03-25 22:53:40 +0530

    vercel.json added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,30 @@
 import './App.css'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
 import { Blog } from './pages/Blog'
 import { Blogs } from './pages/Blogs'
 import { Publish } from './pages/Publish'
+import { Home } from './pages/Home'
 
-function App() {
-  const router = createBrowserRouter([
-    {
-      path:'/signup',
-      element:<Signup/>
-    },
-    {
-      path:'/signin',
-      element:<Signin/>
-    },
-    {
-      path:'/blog/:id',
-      element:<Blog/>
-    },{
-      path:'/blogs',
-      element:<Blogs/>
-    },{
-      path:'/publish',
-      element:<Publish/>
-    }
-  ])
-  return (
-    <>
-      <RouterProvider router={router}/>
-    </>
-  )
-}
+
+  function App() {
+
+    return (
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/blogs" element={<Blogs/>}/>
+            <Route path="/blog/:id" element={<Blog />} />
+            <Route path="/publish" element={<Publish/>}/>
+            <Route path='/' element ={<Home></Home>}/>
+          </Routes>
+        </BrowserRouter>
+      </>
+    )
+  }
+
 
 export default App
